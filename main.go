30da527fe83b7388cb31c825ea2cdab422813fce
Soshi Katsuta <skatsuta@amazon.com>
2017-10-11 12:47:27 +0900

    add Host and Cache-Control headers and print request/response header when verbose

--- main.go
+++ main.go
@@ -10,22 +10,28 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	"github.com/k0kubun/pp"
 )
 
 func main() {
 	// Flags
 	url := flag.String("u", "", "endpoint URL of API Gateway")
-	credPath := flag.String("c", "", "shared credentials file path")
+	credPath := flag.String("c", "", "shared credentials file path (default is ~/.aws/credentials)")
 	profile := flag.String("p", "default", "profile name of credentials")
 	region := flag.String("r", "us-east-1", "API region")
 	method := flag.String("m", "GET", "HTTP method")
 	verbose := flag.Bool("v", false, "verbose output")
+	host := flag.String("host", "", "Host header (default is extracted from URL)")
 	flag.Parse()
 
 	// Create a new HTTP request
 	req, err := http.NewRequest(*method, *url, nil)
 	if err != nil {
 		log.Fatalf("failed to create a new HTTP request: %v", err)
+	}
+
+	if *host != "" {
+		req.URL.Host = *host
 	}
 
 	// Sign the request with the given credentials
@@ -35,9 +41,12 @@ func main() {
 		log.Fatalf("failed to sign a HTTP request: %v", err)
 	}
 
+	// Add no cache header
+	req.Header.Add("Cache-Control", "no-cache")
+
 	// Print Authorization header for debugging
 	if *verbose {
-		log.Println("Authorization:", req.Header.Get("Authorization"))
+		pp.Println("Request header:", req.Header)
 	}
 
 	// Send the HTTP request
@@ -48,6 +57,10 @@ func main() {
 	}
 
 	defer resp.Body.Close()
+
+	if *verbose {
+		pp.Println("Response header:", resp.Header)
+	}
 
 	// Print a response to stdout
 	io.Copy(os.Stdout, resp.Body)
